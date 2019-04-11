ef5d3453898321f88854f1dd55f6a531f8062728
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-04-11 16:16:42 -0500

    #5 Image Component
    SVG image in the home component

--- components/Views/Home.js
+++ components/Views/Home.js
@@ -2,12 +2,29 @@ import React from "react";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles(theme => ({
-  height: {
-    height: "85vh !important"
+  size: {
+    height: '80vh',
+    position: 'relative',
+    overflow: 'hidden'
+  },
+  imageSize: {
+    maxWidth: '100%',
+    width: '100vh',
+    position: 'absolute',
+    bottom: '-10px',
+    right: '-40px',
+    [theme.breakpoints.up('sm')]: {
+      bottom: '-75px',
+      right: '-100px',
+    },
   }
 }));
 
 export default function Home() {
   const classes = useStyles();
-  return <div className={classes.height}>Home</div>;
+  return (
+    <div className={classes.size}>
+        <img className={classes.imageSize} src={require('../../static/marinavv-isotipo.svg')} />
+    </div>
+  );
 }
