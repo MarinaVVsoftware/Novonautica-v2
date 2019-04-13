fe2340219ba7c10daea3ae91fdcf376cc2b69a3e
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-04-13 10:33:41 -0500

    instalación y configuración de variables de entorno

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 const withCSS = require("@zeit/next-css");
 const withImages = require("next-images");
+require("dotenv").config();
+const webpack = require("webpack");
 
 // adecuación para ser deployeado en now.sh
 module.exports = withImages(
   withCSS({
-    target: "serverless"
+    target: "serverless",
+    webpack: config => {
+      config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+      return config;
+    }
   })
 );
