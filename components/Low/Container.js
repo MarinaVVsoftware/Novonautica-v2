cbe1e1151c4c7edd55cf2bfd3e4f9d122d7a81d0
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-04-17 08:06:16 -0500

    #21 Container

---
+++ components/Low/Container.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        ...theme.mixins.gutters(),
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2
+    },
+})) 
+
+function Container(props) {
+    const classes = useStyles();
+
+    <Paper className={classes.root} elevation={1} >
+        {props.children}
+    </Paper>
+}
+
+export default Container;
