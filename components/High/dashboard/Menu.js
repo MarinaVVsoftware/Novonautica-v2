2662b802027d77fa45640608db6404ef0ec7d7ae
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-04-18 10:37:43 -0500

    #22 Combobox component and keys fix

--- components/High/dashboard/Menu.js
+++ components/High/dashboard/Menu.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { makeStyles } from "@material-ui/styles";
-import Divider from "@material-ui/core/Divider";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import PropTypes from "prop-types";
-import ExitApp from "@material-ui/icons/ExitToApp";
-import MenuItem from "./../../Low/MenuItem";
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import PropTypes from 'prop-types';
+import ExitApp from '@material-ui/icons/ExitToApp';
+import MenuItem from './../../Low/MenuItem';
 
-const useStyles = makeStyles(theme => ({
-  color: {
-    color: "#e7e7e7 !important"
-  },
-  colorRed: {
-    color: "#E53935 !important"
-  }
+const useStyles = makeStyles((theme) => ({
+	color: {
+		color: '#e7e7e7 !important'
+	},
+	colorRed: {
+		color: '#E53935 !important'
+	}
 }));
 
 /**
@@ -25,41 +25,37 @@ const useStyles = makeStyles(theme => ({
  * @param {func} handleModuleChange Función para cambiar componente.
  */
 function MenuComponent(props) {
-  const classes = useStyles();
-  return (
-    <div>
-        <Divider />
-            <List>
-                {props.menu.modules.map((item, index) => (
-                    <MenuItem 
-                        menuItem={item} 
-                        key={item} 
-                        handleDrawerClose={props.handleDrawerClose} 
-                        handleModuleChange={props.handleModuleChange} 
-                    />
-                ))}
-            </List>
-        <div>
-            <Divider />
-            <ListItem 
-                button 
-                key={'exit'} 
-                onClick={() => handleClick(props.handleDrawerClose)}
-            >
-                <ListItemIcon className={classes.colorRed}>
-                    <ExitApp />
-                </ListItemIcon>
-                <ListItemText classes = {{ primary: classes.color }} primary={"Salir"} />  
-            </ListItem>
-        </div>
-    </div>
-  );
+	const classes = useStyles();
+	return (
+		<div>
+			<Divider />
+			<List>
+				{props.menu.modules.map((item, index) => (
+					<MenuItem
+						menuItem={item}
+						key={index}
+						handleDrawerClose={props.handleDrawerClose}
+						handleModuleChange={props.handleModuleChange}
+					/>
+				))}
+			</List>
+			<div>
+				<Divider />
+				<ListItem button key={'exit'} onClick={() => handleClick(props.handleDrawerClose)}>
+					<ListItemIcon className={classes.colorRed}>
+						<ExitApp />
+					</ListItemIcon>
+					<ListItemText classes={{ primary: classes.color }} primary={'Salir'} />
+				</ListItem>
+			</div>
+		</div>
+	);
 }
 
 MenuComponent.proptype = {
-    menu: PropTypes.object,
-    handleDrawerClose: PropTypes.func.isRequired,
-    handleModuleChange: PropTypes.func.isRequired,
+	menu: PropTypes.object,
+	handleDrawerClose: PropTypes.func.isRequired,
+	handleModuleChange: PropTypes.func.isRequired
 };
 
 export default MenuComponent;
---
+++ components/Low/ComboBox.js
@@ -0,0 +1,112 @@
+import React, { useEffect } from 'react';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FilledInput from '@material-ui/core/FilledInput';
+import Select from '@material-ui/core/Select';
+import { makeStyles } from '@material-ui/styles';
+import PropTypes from 'prop-types';
+
+const useStyles = makeStyles((theme) => ({
+	root: {},
+	formControl: {
+		margin: theme.spacing.unit,
+		minWidth: 120,
+		backgroundColor: '#424242',
+		'&:disabled': {
+			backgroundColor: 'green !important'
+		},
+		borderRadius: '4px'
+	},
+	filledInput: {
+		color: '#E7E7E7 !important'
+	},
+	filledInputFocusesd: {
+		color: '#2086C7 !important'
+	},
+	inputLabel: {
+		color: '#646464'
+	},
+	underline: {
+		/* estilos del underline: despues de focus */
+		'&:after': {
+			borderBottom: '2px solid #2086C7'
+		},
+		/* bug con estilos. esto lo arrega */
+		'&:before': {
+			borderBottom: '#707070 !important'
+		},
+		/* reemplaza un estilo que causa bug */
+		'&:hover:not($disabled):not($error):not($focused):before': {
+			borderBottom: '1px #707070 !important'
+		}
+	}
+}));
+
+/**
+ * @param {array} options Array con todas las opciones
+ * @param {string} title Titulo para el label de arriba
+ * @param {func} comboBoxValue Funcion para que el padre recupere el objecto actual
+ * @param {bool} disabled Deshabilita el combobox 
+ */
+function Combobox(props) {
+	const classes = useStyles();
+	const [ value, setValue ] = React.useState({
+		option: props.options[0]
+	});
+
+	const handleChange = (event) => {
+		setValue({ ...value, option: event.target.value });
+	};
+
+	useEffect(
+		() => {
+			props.comboBoxValue(value);
+		},
+		[ value ]
+	);
+
+	return (
+		<FormControl variant="filled" className={classes.formControl}>
+			<InputLabel
+				htmlFor="filled-age-simple"
+				className={classes.inputLabel}
+				classes={{ focused: classes.filledInputFocusesd }}
+			>
+				{props.title}
+			</InputLabel>
+			<Select
+				value={value.option}
+				disabled={props.disabled ? true : false}
+				onChange={handleChange}
+				input={
+					<FilledInput
+						name="combobox"
+						id="filled-combobox"
+						className={classes.filledInput}
+						className={classes.filledInput}
+						classes={{
+							underline: classes.underline
+						}}
+					/>
+				}
+			>
+				{props.options.map((option) => (
+					<MenuItem key={option} value={option}>
+						{option}
+					</MenuItem>
+				))}
+			</Select>
+		</FormControl>
+	);
+}
+
+Combobox.propType = {
+	options: PropTypes.array.isRequired,
+	title: PropTypes.string.isRequired,
+	comboBoxValue: PropTypes.func.isRequired,
+	disabled: PropTypes.bool
+};
+
+export default Combobox;
