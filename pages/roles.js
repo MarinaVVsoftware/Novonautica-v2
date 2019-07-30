0f00cb90d67903869944ef6d4b69ed5070304601
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-07-30 08:59:38 -0500

    Fixes

---
+++ pages/roles.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../src/global.css";
+import DashBoard from "../components/Views/DashBoard";
+import Roles from "../components/Views/Roles";
+
+function roles(props) {
+  if (!props) return null;
+  return (
+    <DashBoard
+      name={props.data.user.username}
+      menu={JSON.parse(props.data.user.rol.permissions)}>
+      <Users />
+    </DashBoard>
+  );
+}
+
+export default roles;
